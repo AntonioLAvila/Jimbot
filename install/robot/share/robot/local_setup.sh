8fc2863c5540faa46ef6ed529db3daf32f27d0df
Antonio Avila <antonioavila433@gmail.com>
2023-01-21 15:56:11 -0500

    it works :)

--- install/robot/share/robot/local_setup.sh
+++ install/robot/share/robot/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/antonio/Documents/GitHub/Jimbot/install/robot"}
+: ${AMENT_CURRENT_PREFIX:="/home/antonioavila/Documents/GitHub/Jimbot/install/robot"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
--- src/robot/src/drive.cpp
+++ src/robot/src/drive.cpp
@@ -2,7 +2,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include <geometry_msgs/msg/vector3.hpp>
-#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/joy.hpp>
 #include <string.h>
 #include <bits/stdc++.h>
 
@@ -15,18 +15,19 @@ class MinimalSubscriber : public rclcpp::Node
     MinimalSubscriber()
     : Node("minimal_subscriber")
     {
-      subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
-      "/camera/gyro/sample", rclcpp::SensorDataQoS(), std::bind(&MinimalSubscriber::topic_callback, this, _1));
+      subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(//sensor_msgs::msg::Imu
+      "/joy"/*"/camera/gyro/sample"*/, rclcpp::SensorDataQoS(), std::bind(&MinimalSubscriber::topic_callback, this, _1));
     }
 
   private:
-    void topic_callback(const sensor_msgs::msg::Imu & msg) const
+    void topic_callback(const sensor_msgs::msg::Joy & msg) const
     {
-      geometry_msgs::msg::Vector3 v = msg.angular_velocity;
+      // geometry_msgs::msg::Vector3 v = msg.angular_velocity;
       
-      RCLCPP_INFO(this->get_logger(), to_string(v.x).c_str());
+      // RCLCPP_INFO(this->get_logger(), to_string(v.x).c_str());      
+      RCLCPP_INFO(this->get_logger(), to_string(msg.axes[0]).c_str());
     }
-    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
