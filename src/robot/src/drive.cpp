b180d566c0d1b7da0df4d048130f34d4a8882d4c
Antonio Avila <antonioavila433@gmail.com>
2023-01-30 21:02:43 -0500

    In the middle of adding serial output to the arduino

--- src/robot/src/drive.cpp
+++ src/robot/src/drive.cpp
@@ -1,58 +1,99 @@
 #include <memory>
 
+#include <stdio.h>
+#include <string.h>
+
 #include "rclcpp/rclcpp.hpp"
 #include <geometry_msgs/msg/vector3.hpp>
 #include <sensor_msgs/msg/joy.hpp>
 #include <string.h>
 #include <bits/stdc++.h>
 
-#include "glm/vec2.hpp"
-#include "glm/glm.hpp"
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <errno.h> // Error integer and strerror() function
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <unistd.h> // write(), read(), close()
 
 class MinimalSubscriber : public rclcpp::Node
 {
   public:
     MinimalSubscriber()
-    : Node("minimal_subscriber") {
-      subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(//sensor_msgs::msg::Imu
-      "/joy"/*"/camera/gyro/sample"*/, rclcpp::SensorDataQoS(), std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
+    : Node("minimal_subscriber") {/*sensor_msgs::msg::Imu*/
+      subscription_ = this->create_subscription<sensor_msgs::msg::Joy>("/joy"/*"/camera/gyro/sample"*/, rclcpp::SensorDataQoS(), std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
+
+      serial_port = open("/dev/ttyUSB0", O_RDWR);
+
+      // Create new termios struct, we call it 'tty' for convention
+      // struct termios tty;
+
+      // Read in existing settings, and handle any error
+      if(tcgetattr(serial_port, &tty) != 0) {
+          printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+      }
+
+      tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+      tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+      tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
+      tty.c_cflag |= CS8; // 8 bits per byte (most common)
+      tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+      tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+      tty.c_lflag &= ~ICANON;
+      tty.c_lflag &= ~ECHO; // Disable echo
+      tty.c_lflag &= ~ECHOE; // Disable erasure
+      tty.c_lflag &= ~ECHONL; // Disable new-line echo
+      tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+      tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+      tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+
+      tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+      tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+      // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
+      // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
+
+      tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+      tty.c_cc[VMIN] = 0;
+
+      // Set in/out baud rate to be 9600
+      cfsetispeed(&tty, B9600);
+      cfsetospeed(&tty, B9600);
+
+      // Save tty settings, also checking for error
+      if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+          printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+      }
     }
 
-    void updateWheelPowers(glm::vec2 trans, float rot){
-      wheelPowers[0] = glm::dot(trans, wheelUnitVectors[0]) + rot;
-      wheelPowers[1] = glm::dot(trans, wheelUnitVectors[1]) - rot;
-      wheelPowers[2] = glm::dot(trans, wheelUnitVectors[2]) - rot;
-      wheelPowers[3] = glm::dot(trans, wheelUnitVectors[3]) + rot;
+    void updateWheelPowers(float trans[2], float rot){
+      wheelPowers[0] = (trans[0] + trans[1] + rot)/2;
+      wheelPowers[1] = (trans[0] - trans[1] - rot)/2;
+      wheelPowers[2] = (trans[0] + trans[1] - rot)/2;
+      wheelPowers[3] = (trans[0] - trans[1] + rot)/2;
     }
 
-    void setMotors(){
+    void setMotors(float powers[4]){
+      char command[21];
+      sprintf(command, "FL%fFR%fBR%fBL%f", powers[0], powers[1], powers[2], powers[3]);
+      write(serial_port, command, sizeof(command));
     }
 
   private:
     float rotate = 0;
-    glm::vec2 translate = glm::vec2(0,0);
-
-    glm::vec2 wheelUnitVectors[4] = {glm::vec2(1/sqrt(2), 1/sqrt(2)),
-                                    glm::vec2(-1/sqrt(2), 1/sqrt(2)),
-                                    glm::vec2(1/sqrt(2), 1/sqrt(2)),
-                                    glm::vec2(-1/sqrt(2), 1/sqrt(2))};
-    
-    glm::vec2 wheelVectors[4] = {glm::vec2(0,0),
-                                glm::vec2(0,0),
-                                glm::vec2(0,0),
-                                glm::vec2(0,0)};
-
+    float translate[2] = {0,0};
     float wheelPowers[4] = {0,0,0,0};
+    int serial_port;
+    struct termios tty;
 
     void topic_callback(const sensor_msgs::msg::Joy & msg) {
       // geometry_msgs::msg::Vector3 v = msg.angular_velocity;
       
       // RCLCPP_INFO(this->get_logger(), to_string(v.x).c_str());
 
-      translate.x = msg.axes[0];
-      translate.y = msg.axes[1];
+      translate[0] = msg.axes[0];
+      translate[1] = msg.axes[1];
       rotate = msg.axes[3];
       updateWheelPowers(translate, rotate);
+      setMotors(wheelPowers);
       RCLCPP_INFO(this->get_logger(), std::to_string(msg.axes[0]).c_str());
     }
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
